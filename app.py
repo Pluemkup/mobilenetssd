fe669690c5240e14528ade5e78f39a59d1c5ff3f
Pluemkup <89238892+Pluemkup@users.noreply.github.com>
2021-11-18 10:58:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,13 +151,16 @@ def event_handle(event):
         msg = str(event["message"]["text"])
         if (msg == "สวัสดี") :
            replyObj = TextSendMessage(text="ดีด้วย")
+           line_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "กินข้าวกันมั้ย") :
            replyObj = TextSendMessage(text="ขอบคุณ")
+           line_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "เที่ยวไหนดี") :
            replyObj = TextSendMessage(text="บ้านเลยพี่")
+           line_bot_api.reply_message(rtoken, replyObj)
         else :
             replyObj = TextSendMessage(text=msg)
-        line_bot_api.reply_message(rtoken, replyObj)
+            line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
